5861098ed6012241d82f403dd67f3d7a93461507
dnth <dickson.neoh@gmail.com>
2025-04-04 21:36:38 +0800

    formatting

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,7 @@ conf = configure_model(conf, num_queries=100, freeze_at=0, pretrained=True)
 
 conf = configure_dataset(
     config=conf,
-    image_size=(800, 800),
+    image_size=(640, 640),
     train_ann_file="/home/dnth/Desktop/DEIMKit/dataset_collections/Rock Paper Scissors SXSW.v14i.coco/train/_annotations.coco.json",
     train_img_folder="/home/dnth/Desktop/DEIMKit/dataset_collections/Rock Paper Scissors SXSW.v14i.coco/train",
     val_ann_file="/home/dnth/Desktop/DEIMKit/dataset_collections/Rock Paper Scissors SXSW.v14i.coco/valid/_annotations.coco.json",
@@ -15,10 +15,10 @@ conf = configure_dataset(
     val_batch_size=16,
     num_classes=4,
     remap_mscoco=False,
-    output_dir="./outputs/rock-paper-scissors/deim_hgnetv2_s_30ep_640px_num_queries_resume_3",
+    output_dir="./nbs/outputs/rock-paper-scissors/deim_hgnetv2_s_dfine_obj365_s_10ep",
 )
 
 trainer = Trainer(conf)
 
-trainer.load_checkpoint("/home/dnth/Desktop/DEIMKit/outputs/rock-paper-scissors/deim_hgnetv2_s_30ep_640px_num_queries_resume_2/best.pth")
-trainer.fit(epochs=10, save_best_only=True, lr=0.00004)
+trainer.load_checkpoint("/home/dnth/Desktop/DEIMKit/checkpoints/dfine_s_obj365.pth")
+trainer.fit(epochs=10, save_best_only=True)
